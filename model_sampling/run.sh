b3be5b2dcfe37ae9b62a5f3e54500d993e37ce2b
Mingzhe Du <dumingzhe@126.com>
2025-01-23 19:59:09 +0000

    Update model sampling

---
+++ model_sampling/run.sh
@@ -0,0 +1,89 @@
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.7 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "competition" \
+    --split "train"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.5 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "competition" \
+    --split "train"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.3 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "competition" \
+    --split "train"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.7 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "introductory" \
+    --split "test"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.5 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "introductory" \
+    --split "test"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.3 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "introductory" \
+    --split "test"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.7 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "interview" \
+    --split "test"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.5 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "interview" \
+    --split "test"
+
+python model_sampling.py \
+    --task_num 4 \
+    --temperature 0.3 \
+    --batch_number 16 \
+    --batch_size 4 \
+    --model_path "microsoft/Phi-3.5-mini-instruct" \
+    --model_name "Phi_3_5_mini_instruct_vanilla" \
+    --difficulty "interview" \
+    --split "test"
