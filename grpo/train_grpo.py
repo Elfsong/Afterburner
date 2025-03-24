1dabee70d1ccbac38173982718f43862c6153cae
Elfsong <dumingzhex@gmail.com>
2025-03-24 14:06:09 +0800

    Update

--- grpo/train_grpo.py
+++ grpo/train_grpo.py
@@ -1,15 +1,19 @@
+# import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+
+
 import re
 import torch
 from trl import GRPOConfig
 from trl import GRPOTrainer
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM
 from math_verify import LatexExtractionConfig, parse, verify
+
 
 dataset_id = "AI-MO/NuminaMath-TIR"
 train_dataset, test_dataset = load_dataset(dataset_id, split=["train[:5%]", "test[:5%]"])
-tokenizer= AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B-Instruct")
 
 SYSTEM_PROMPT = (
     "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant "
@@ -18,31 +22,29 @@ SYSTEM_PROMPT = (
     "<think> reasoning process here </think><answer> answer here </answer>"
 )
 
+
 def make_conversation(example):
-    messages =  {
+    return {
         "prompt": [
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": example["problem"]},
         ],
     }
-    prompt = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
-    )
-    return prompt
+
 
 train_dataset = train_dataset.map(make_conversation)
 test_dataset = test_dataset.map(make_conversation)
 train_dataset = train_dataset.remove_columns(["messages", "problem"])
 
 
-model_id = "Qwen/Qwen2-0.5B-Instruct"
+model_id = "Qwen/Qwen2.5-Coder-7B-Instruct"
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype="auto",
     device_map="auto",
+    attn_implementation='flash_attention_2'
 )
+
 
 lora_config = LoraConfig(
     task_type="CAUSAL_LM",
@@ -53,15 +55,16 @@ lora_config = LoraConfig(
 )
 
 model = get_peft_model(model, lora_config)
-model.print_trainable_parameters()
 
+def length_reward(completions, **kwargs):
+    completion_contents = [completion[0]["content"] for completion in completions]
+    return [len(completion_content) for completion_content in completion_contents]
 
 def format_reward(completions, **kwargs):
     """Reward function that checks if the completion has a specific format."""
     pattern = r"^<think>.*?</think>\s*<answer>.*?</answer>$"
     completion_contents = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, content) for content in completion_contents]
-    rewards_list = [1.0 if match else 0.0 for match in matches]
     return [1.0 if match else 0.0 for match in matches]
 
 def accuracy_reward(completions, **kwargs):
@@ -82,18 +85,19 @@ def accuracy_reward(completions, **kwargs):
     return rewards
 
 
-
 # Configure training arguments using GRPOConfig
 training_args = GRPOConfig(
-    output_dir="Qwen2-0.5B-GRPO-test",
+    output_dir="Qwen/Qwen2.5-Coder-7B-Instruct-GRPO-test",
     learning_rate=1e-5,
-    remove_unused_columns=False,
-    gradient_accumulation_steps=16,
+    remove_unused_columns=False,  # to access the solution column in accuracy_reward
+    gradient_accumulation_steps=4,
     num_train_epochs=1,
-    bf16=True,
-    max_completion_length=64,  # default: 256
+    bf16=False,
+    # Parameters that control de data preprocessing
+    max_completion_length=2048,  # default: 256
     num_generations=4,  # default: 8
-    max_prompt_length=128,  # default: 512
+    max_prompt_length=512,  # default: 512
+    # Parameters related to reporting and saving
     report_to=["wandb"],
     logging_steps=1,
     push_to_hub=True,
@@ -102,12 +106,8 @@ training_args = GRPOConfig(
 )
 
 
-
 trainer = GRPOTrainer(
-    model=model, reward_funcs=[format_reward, accuracy_reward], args=training_args, train_dataset=train_dataset
+    model=model, reward_funcs=[format_reward, accuracy_reward, length_reward], args=training_args, train_dataset=train_dataset
 )
 
 trainer.train()
-
-trainer.save_model(training_args.output_dir)
-trainer.push_to_hub(dataset_name=dataset_id)
