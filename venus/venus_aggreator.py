bba2e32fa15f40365e1410163e881ef407fe59ae
Elfsong <dumingzhex@gmail.com>
2025-04-09 01:19:05 +0800

    Update

--- venus/venus_aggreator.py
+++ venus/venus_aggreator.py
@@ -1,9 +1,8 @@
-# %%
 import json
 from tqdm import tqdm
 from datasets import load_dataset, Dataset
 
-# %%
+
 data = list()
 for index in tqdm(range(100)):
     try:
@@ -13,10 +12,8 @@ for index in tqdm(range(100)):
     except Exception as e:
         print(f"Error loading dataset for index {index}: {e}")
 
-# %%
-ds = Dataset.from_list(data)
 
-# %%
+ds = Dataset.from_list(data)
 ds.push_to_hub('Elfsong/Venus_python_verified', split='verified', private=True)
 
 
